75029f526e6690c7535191f7cbde2cd5e0912bd1
Nariman Hasanzade <qasanzade545@gmail.com>
2023-04-29 22:17:32 +0400

    Написал я код через return, и через console, то есть у меня два решения)

--- 0-module/1-task/index.js
+++ 0-module/1-task/index.js
@@ -1,3 +1,14 @@
 function sum(m, n) {
-  // ваш код...
+  return m + n;
 }
+let result = sum(1, 1);
+let result1 = sum(2, 5);
+alert( result );
+alert( result1 ); // ваш код...
+
+
+function sum(m , n) {
+  return m + n;
+}
+console.log(sum(1,1));
+console.log(sum(2, 5));  // ваш код...
