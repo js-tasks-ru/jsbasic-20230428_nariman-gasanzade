11f08d754ddcbb28ab45b237dd9ff318068ff1fc
Nariman Hasanzade <qasanzade545@hmail.com>
2023-05-01 11:28:12 +0400

    Added saHi

--- 0-module/1-task/index.js
+++ 0-module/1-task/index.js
@@ -1,14 +1,5 @@
-function sum(m, n) {
-  return m + n;
-}
-let result = sum(1, 1);
-let result1 = sum(2, 5);
-alert( result );
-alert( result1 ); 
-
-
 function sum(m , n) {
   return m + n;
 }
 console.log(sum(1,1));
-console.log(sum(2, 5));  
+console.log(sum(2, 5)); 
