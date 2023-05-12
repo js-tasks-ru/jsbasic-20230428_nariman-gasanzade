3edd292f8d8c493e7b6982b46dc308ec6c761d0a
Nariman Hasanzade <qasanzade545@hmail.com>
2023-05-12 20:05:15 +0400

    r

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,20 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let totalSalary = 0;
+  for (let key in salaries) {
+    if (typeof salaries[key] === 'number' && !isNaN(salaries[key]) && isFinite(salaries[key])) {
+      totalSalary += salaries[key];
+    }
+  }
+  return totalSalary;
 }
+
+let salaries = {
+  John: 1000,
+  Ann: 1600,
+  Pete: 1300,
+  month: 'December',
+  currency: 'USD',
+  isPayed: false
+}
+
+console.log(sumSalary(salaries)); 
