5ee46aa0aba37719c94e3764a0287080941046e4
Nariman Hasanzade <qasanzade545@hmail.com>
2023-05-26 17:32:49 +0400

    упрошение

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -3,11 +3,7 @@ function toggleText() {
   const button = document.querySelector('.toggle-text-button');
   
   button.addEventListener('click', () => {
-    if (text.hasAttribute('hidden')) {
-      text.removeAttribute('hidden');
-    } else {
-      text.setAttribute('hidden', '');
-    }
+    text.hidden = !text.hidden;
   });// ваш код...
 }
 
