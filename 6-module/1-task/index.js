8e97c48e47591856fe8e093fd3996a92a56fc391
Nariman Hasanzade <qasanzade545@hmail.com>
2023-06-01 17:11:22 +0400

    js

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,35 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.elem = document.createElement('table');
+    this.elem.innerHTML = `
+      <thead>
+        <tr>
+          <th>Имя</th>
+          <th>Возраст</th>
+          <th>Зарплата</th>
+          <th>Город</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+      </tbody>
+    `;
+    const tbody = this.elem.querySelector('tbody');
+    for (const row of rows) {
+      const tr = document.createElement('tr');
+      tr.innerHTML = `
+        <td>${row.name}</td>
+        <td>${row.age}</td>
+        <td>${row.salary}</td>
+        <td>${row.city}</td>
+        <td><button>X</button></td>
+      `;
+      const button = tr.querySelector('button');
+      button.addEventListener('click', () => {
+        tr.remove();
+      });
+      tbody.appendChild(tr);
+    }
   }
 }
